03204606ddbf17bd6b51e1daf4ba1a1f61ab4ce9
Rza-Nagpal <24bai70183@cuchd.in>
2026-07-10 13:57:10 +0530

    Time: 74 ms (57.66%) | Memory: 93.7 MB (80.82%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,16 +1,36 @@
+// class Solution {
+// public:
+//     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//         unordered_map<int, int> mp;
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (mp.count(nums[i]) && (i - mp[nums[i]] <= k)) {
+//                 return true;
+//             }
+
+//             mp[nums[i]] = i;
+//         }
+
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (mp.count(nums[i]) && (i - mp[nums[i]] <= k)) {
+        unordered_set<int> mp;
+         for(int i = 0; i < nums.size(); i++) {
+            if(i>k)
+            {
+                mp.erase(nums[i-k-1]);
+            }
+            if(mp.find(nums[i])!=mp.end())
+            {
                 return true;
             }
-
-            mp[nums[i]] = i;
-        }
-
-        return false;
-    }
+            mp.insert(nums[i]);
+            }
+            return false;
+            
+            }
 };
